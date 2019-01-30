a161f8467dead9772c7d7b80fc1c8837b405f1f1
Denny Le and Yulia Shidlovskaya <yulia@>
2019-01-30 15:04:04 -0800

    add a second dice

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,6 @@
-function Game() {
-  this.players = [],
-  this.playerId = 0
-}
-
-Game.prototype.addPlayer = function(player) {
-  this.players.push(player);
-  player.id = this.playerId;
-  this.playerId += 1;
-}
-
-Game.prototype.findPlayer = function(id) {
-  for (var i = 0; i <this.players.length; i++) {
-    if (this.players[i]) {
-      if (this.players[i].id == id) {
-        return this.players[i];
-      }
-    }
-  };
-  return false;
-}
-
 function Player(){
   this.score = 0,
   this.roundScore = 0
-}
-
-Player.prototype.clearRoundScore = function() {
-  this.roundScore = 0;
 }
 
 Player.prototype.Hold = function() {
@@ -34,34 +8,91 @@ Player.prototype.Hold = function() {
   this.roundScore = 0;
 }
 
-Player.prototype.RollTheDie = function(dieScore) {
-  if (dieScore == 1) {
+Player.prototype.RollTheDie = function(diceScore, hasOne) {
+  if (hasOne == true) {
     this.roundScore = 0;
     return false;
   } else {
-    this.roundScore += dieScore;
+    this.roundScore += diceScore;
     if (this.roundScore + this.score >= winningScore){
       this.score += this.roundScore;
       return this.score;
-
     }
     return this.roundScore;
   }
 };
+
+function Die(){
+  this.point = 0
+}
+
+Die.prototype.roll = function() {
+  this.point = getRandom();
+  return this.point;
+}
+
+function Dice(){
+  this.totalPoints = 0,
+  this.hasOne = false,
+  this.dice = [],
+  this.currentId = 0
+}
+
+Dice.prototype.reset = function() {
+  this.totalPoints = 0;
+  this.hasOne = false;
+}
+
+Dice.prototype.addDie = function(die) {
+  die.id = this.currentId;
+  this.currentId += 1;
+  this.dice.push(die);
+}
+
+Dice.prototype.findDie = function(id) {
+  for (var i=0; i< this.dice.length; i++) {
+    if (this.dice[i]) {
+      if (this.dice[i].id == id) {
+        return this.dice[i];
+      }
+    }
+  };
+  return false;
+}
+
+Dice.prototype.start = function() {
+  for (var i=0; i< this.dice.length; i++) {
+    if (this.dice[i]) {
+      var diePoints = this.dice[i].roll();
+      if (diePoints === 1){
+        this.hasOne = true;
+      } else {
+        this.totalPoints += diePoints;
+      }
+    }
+  };
+  return false;
+}
+
 function getRandom() {
-  return Math.floor(Math.random() * (7 - 1) + 1);
+  return Math.floor(Math.random() * 6 + 1);
 };
 
-var player1 = new Player();
-var player2 = new Player();
-var winningScore = 20;
-
 function emptyRoundScore() {
-  $("#die").text("0");
   $("#roundScore").text("0");
 }
 
-function scoreCheck(result, userId){
+function emptyDiceScore() {
+  $("#die1").text("0");
+  $("#die2").text("0");
+}
+
+var player1 = new Player();
+var player2 = new Player();
+var winningScore = 100;
+var allDice = new Dice();
+
+function displayPlayerScore(result, userId){
   var nextId = userId + 1;
   if (userId === 2) {
     nextId = 1;
@@ -69,49 +100,74 @@ function scoreCheck(result, userId){
   if (result >= winningScore){
     alert("Player " + userId + " wins!");
     $("#reset").show();
-    $("#play").hide();
+    disableButtons(1);
+    disableButtons(2);
+
   } else if (result === false) {
-    disableButtons(userId, nextId);
+    disableButtons(userId);
+    enableButtons(nextId);
     emptyRoundScore();
+    displayDiePoints(allDice);
     alert("Player " + nextId + "'s turn.");
   } else {
     $("#roundScore").text(result);
   }
 }
 
-function disableButtons(id1, id2){
-  $("#rollDie" + id1).prop("disabled",true);
-  $("#hold" + id1).prop("disabled",true);
-  $("#rollDie" + id2).prop("disabled",false);
-  $("#hold" + id2).prop("disabled",false);
+function disableButtons(id) {
+  $("#rollDie" + id).prop("disabled",true);
+  $("#hold" + id).prop("disabled",true);
+}
+function enableButtons(id) {
+  $("#rollDie" + id).prop("disabled",false);
+  $("#hold" + id).prop("disabled",false);
 }
 
+function displayDiePoints(dice){
+  dice.dice.forEach(function(die){
+    var tagId = parseInt(die.id) + 1;
+    $("#die"+ tagId).text(die.point);
+  })
+};
+
 $(document).ready(function() {
+
+  var die1 = new Die();
+  var die2 = new Die();
+  allDice.addDie(die1);
+  allDice.addDie(die2);
+
   $("#rollDie1").click(function() {
-    var dieScore = getRandom();
-    $("#die").text(dieScore);
-    var result = player1.RollTheDie(dieScore);
-    scoreCheck(result, 1);
+    allDice.start();
+    displayDiePoints(allDice);
+    console.log(allDice);
+    var result = player1.RollTheDie(allDice.totalPoints, allDice.hasOne);
+    displayPlayerScore(result, 1);
+    allDice.reset();
   });
 
   $("#hold1").click(function() {
     player1.Hold();
     $("#score1").text(player1.score);
-    disableButtons(1, 2);
+    disableButtons(1);
+    enableButtons(2);
     emptyRoundScore();
   });
 
   $("#rollDie2").click(function() {
-    var dieScore = getRandom();
-    $("#die").text(dieScore);
-    var result = player2.RollTheDie(dieScore);
-    scoreCheck(result, 2);
+    allDice.start();
+    displayDiePoints(allDice);
+    console.log(allDice);
+    var result = player2.RollTheDie(allDice.totalPoints, allDice.hasOne);
+    displayPlayerScore(result, 2);
+    allDice.reset();
   });
 
   $("#hold2").click(function() {
     player2.Hold();
     $("#score2").text(player2.score);
-    disableButtons(2, 1);
+    disableButtons(2);
+    enableButtons(1);
     emptyRoundScore();
   });
 
